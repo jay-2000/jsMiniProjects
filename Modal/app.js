1679c21d9acf9997f0bd35908be348e799382a35
jay-2000 <jayparmar7654321@gmail.com>
2021-12-26 23:43:48 +0530

    added logic for js in modal

--- Modal/app.js
+++ Modal/app.js
@@ -4,4 +4,12 @@
 // when user clicks close-btn remove .open-modal from modal-overlayx
 
 const modalBtn = document.querySelector(".modal-btn");
-const modalOverlay = document.querySelector(".modal-overlay")
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click",function(){
+modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click",function(){
+modal.classList.remove("open-modal");
+});
